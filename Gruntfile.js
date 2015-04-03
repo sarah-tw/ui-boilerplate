064dbe6756600c0347f9be950f27d756212c110f
Sarah <sixiaojing1988@gmail.com>
2015-04-03 11:46:50 +0800

    change the structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,20 +5,78 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     watch: {
-      files: ['src/**/*.less', 'index.html', 'src/javascripts/*.*'],
+      files: ['assets/**/*.less', 'index.html', 'assets/javascripts/*.*'],
       tasks: ['default'],
       options: {
         livereload: true
       },
     },
 
+    clean: {
+        assets: {
+            src: ['build/**'],
+            options: {
+              force: true
+            }
+        }
+    },    
+    copy: {
+      assets: {
+        files: [{
+          expand: true,
+          cwd: 'assets/less',
+          src: '*.less',
+          dest: 'build/stylesheets/less/'
+        },
+        {
+          expand: true,
+          cwd: 'assets/images',
+          src: '*.*',
+          dest: 'build/images/'
+        },
+        ]
+      }
+    },
+
     less: {
       all: {
+        options: {
+            paths: ['../../bower_components/bootstrap/less'],
+            sourceMap: true,
+            sourceMapFilename: 'build/stylesheets/main.css.map',
+            sourceMapURL: 'main.css.map',
+            sourceMapBasepath: 'build/stylesheets',
+            sourceMapRootpath: '/build/stylesheets/less',
+            compress: true
+        },        
         files: {
-          "main.css": "src/less/main.less"
+          "build/stylesheets/main.css": "assets/less/main.less"
         }
       }
     },
+    uglify: {
+      my_target: {
+        files: {
+          'build/javascript/main.min.js': ['assets/**/*.js']
+        }
+      }
+    },
+    // compress: {
+    //     images: {
+    //         options: {'mode': 'gzip'},
+    //         expand: true,
+    //         cwd: 'assets/',
+    //         src: 'images/*.*',
+    //         dest: 'build/'
+    //     }
+    //     javascript: {
+    //         options: {'mode': 'gzip'},
+    //         expand: true,
+    //         cwd: "build/javascripts",
+    //         src: '../play-app/public/javascripts/*.js'//,
+    //     }
+    // },    
+
     webdriver: {
       options: {
           host: 'localhost',
